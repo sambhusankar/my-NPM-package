5754fa2e8fa48b69f68390c8dbc11fbfecd290bc
Developer Sankar <sankar@sankar.sankar>
2025-01-17 21:25:03 +0530

    package published

---
+++ index.js
@@ -0,0 +1,35 @@
+// index.js
+
+// Function to add numbers
+function sum(...numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
+
+// Function to subtract numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// Function to multiply numbers
+function multiply(...numbers) {
+    return numbers.reduce((acc, num) => acc * num, 1);
+}
+
+// Function to find the maximum number
+function max(...numbers) {
+    return Math.max(...numbers);
+}
+
+// Function to find the minimum number
+function min(...numbers) {
+    return Math.min(...numbers);
+}
+
+// Export functions
+module.exports = {
+    sum,
+    subtract,
+    multiply,
+    max,
+    min,
+};
---
+++ test.js
@@ -0,0 +1,9 @@
+// test.js
+
+const mathUtils = require('./index');
+
+console.log('Sum:', mathUtils.sum(1, 2, 3, 4)); // 10
+console.log('Subtract:', mathUtils.subtract(10, 5)); // 5
+console.log('Multiply:', mathUtils.multiply(2, 3, 4)); // 24
+console.log('Max:', mathUtils.max(1, 2, 3, 4)); // 4
+console.log('Min:', mathUtils.min(1, 2, 3, 4)); // 1
